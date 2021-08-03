03ce1281df0e97e2be9b2620f3a5d0b508bda891
Souravgupta959@gmail.com <souravgupta959@yahoo.com>
2021-08-04 00:24:31 +0530

    Three numbers sum

--- sum.java
+++ sum.java
@@ -1,8 +1,8 @@
 Class sum{
 	public static void main(String args[]){
 		int num1=10;
-		int num2=20;
-		int sum=num1+num2;
+		int num2=20,num3=50;
+		int sum=num1+num2+num3;
 		System.out.println(sum);
 		System.out.println("Thank You For Watching");
 	}
