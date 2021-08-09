c6fac99ddf1f6e8b218619f3dfc8b1e5aeb2925a
Souravgupta959@gmail.com <souravgupta959@yahoo.com>
2021-08-10 00:35:12 +0530

    new

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 Class sum{
 	public static void main(String args[]){
 		int num1=10;
-		int num2=20,num3=50;
+		int num2=20,num3=50,num4=100,num5=500;
 		int sum=num1+num2+num3;
 		System.out.println(sum);
 		System.out.println("Thank You For Watching");
